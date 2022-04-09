2eb28ed6f2da602622a0cb1de586680fa4d4bc89
joaolucas_lacuna <joaop@lacunasoftware.com>
2022-04-08 22:28:10 -0300

    feat: adding tests

--- api-calculator/src/index.js
+++ api-calculator/src/index.js
@@ -1,30 +1,33 @@
-import express from "express";
+const express = require("express");
+const Calculator = require("./service/calculatorService.js");
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/sum", (req, res) => {
+const calc = new Calculator();
+
+app.post("/add", (req, res) => {
   const { a, b } = req.body;
-  res.json({ result: a + b });
+  res.json({ result: calc.add(a, b) });
 });
 
 app.post("/sub", (req, res) => {
   const { a, b } = req.body;
-  res.json({ result: a - b });
+  res.json({ result: calc.sub(a, b) });
 });
 
 app.post("/mul", (req, res) => {
   const { a, b } = req.body;
-  res.json({ result: a * b });
+  res.json({ result: calc.mul(a, b) });
 });
 
 app.post("/div", (req, res) => {
   const { a, b } = req.body;
-  res.json({ result: a + b });
+  res.json({ result: calc.div(a, b) });
 });
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.json({
     description:
       "It's possible perform sum, sub, div and mul operations, just make a post request and pass the a and b values",
---
+++ api-calculator/test/calculator.test.js
@@ -0,0 +1,51 @@
+const Calculator = require("../src/service/calculatorService.js");
+
+// create mockCalculator
+const mockCalculator = {
+    result: 0,
+    add(a, b) {
+        this.result = a + b;
+        return this.result;
+    }
+}
+
+describe('Calculator Mock', () => {
+    it("add", () => {
+        const calc = mockCalculator;
+        const result = calc.add(1, 2);
+        expect(result).toBe(3);
+    });
+});
+
+describe('Calculator basic operations', () => {
+    it("Test add is correct", () => {
+        const calculator = new Calculator();
+        const result = calculator.add(1, 2);
+        expect(result).toBe(3);
+    });
+
+    it("Test sub is correct", () => {
+        const calculator = new Calculator();
+        const result = calculator.sub(1, 2);
+        expect(result).toBe(-1);
+    });
+
+    it("Test mul is correct", () => {
+        const calculator = new Calculator();
+        const result = calculator.mul(1, 2);
+        expect(result).toBe(2);
+    });
+
+    it("Test div is correct", () => {
+        const calculator = new Calculator();
+        const result = calculator.div(1, 2);
+        expect(result).toBe(0.5);
+    });
+
+    it("Test div is throw error", () => {
+        const calculator = new Calculator();
+        expect(() => {
+            calculator.div(1, 0);
+        }).toThrowError("Division by zero is not allowed");
+    });
+})
